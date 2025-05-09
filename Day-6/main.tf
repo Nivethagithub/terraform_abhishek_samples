e5907d80efbf549b6335d7c6c2005d0431f29da3
Nivethagithub <59144972+Nivethagithub@users.noreply.github.com>
2025-05-09 18:54:27 +0530

    Update main.tf

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -22,3 +22,49 @@ module "ec2_instance" {
   ami = var.ami
   instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
 }
+
+
+##### Below code to deploy in different regions as different env deployment. ########
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-west-2"
+  alias = "us-west-2"
+}
+
+variable "ami" {
+  description = "value"
+}
+
+variable "instance_type" {
+  description = "value"
+  type = map(string)
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+    "prod" = "t2.xlarge"
+  }
+}
+
+module "ec2_instance_us_east_1" {
+  providers = {
+    aws = aws.us-east-1
+  }
+  source = "./modules/ec2_instance"
+  ami = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+}
+
+module "ec2_instance_us_west_2" {
+  providers = {
+    aws = aws.us-west-2
+  }
+  source = "./modules/ec2_instance"
+  ami = var.ami
+  instance_type = lookup(var.instance_type, terraform.workspace, "t2.micro")
+}
